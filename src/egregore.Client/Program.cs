e358f1e27bf82ff7442209d85c939c43e23f5a23
Daniel Crenna <daniel.crenna@gmail.com>
2020-08-10 21:31:20 -0400

    relocate client files to blazor app

--- src/egregore.Client/Program.cs
+++ src/egregore.Client/Program.cs
@@ -12,9 +12,7 @@ namespace egregore.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             await builder.Build().RunAsync();
         }
     }
