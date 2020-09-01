188a69ace7c0ac98a2f10ad36133af534776af07
Daniel Crenna <daniel.crenna@gmail.com>
2020-08-31 21:38:18 -0400

    add topo sort

---
+++ test/egregore.Tests/TopologicalSorterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace egregore.Tests
+{
+    public class TopologicalSorterTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TopologicalSorterTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Can_sort()
+        {
+            var list = new List<string> {"A", "B", "C"};
+
+            var edges = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("C", "A"),  // A depends on C
+                new Tuple<string, string>("B", "C")   // C depends on B
+            };
+            
+            var sorted = TopologicalSorter<string>.Sort(list, edges);
+            Assert.NotNull(sorted);
+
+            foreach(var entry in sorted)
+                _output.WriteLine(entry);
+        }
+    }
+}
