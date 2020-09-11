e9ce49739d9bfcb4546ac610ce5ff1c838579d2f
Daniel Crenna <daniel.crenna@gmail.com>
2020-09-10 20:51:07 -0400

    add logging UI

---
+++ src/egregore.Client/Shared/Table.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace egregore.Client.Shared
+{
+    public sealed class Table : ComponentBase, IDisposable
+    {
+        public Table()
+        {
+            
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder b)
+        {
+            b.OpenElement(0, HtmlTags.Table);
+            b.AddAttribute(1, HtmlAttributes.Class, "w-auto");
+            b.CloseElement();
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            return base.OnAfterRenderAsync(firstRender);
+        }
+        
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+        protected override Task OnParametersSetAsync()
+        {
+            return base.OnParametersSetAsync();
+        }
+
+        public void Dispose() { }
+    }
+}
